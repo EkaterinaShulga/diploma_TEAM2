8cd49613e78157667fcfb8bb00ec6146deb66e32
Leonid Bagrin <bgrln347@gmail.com>
2023-01-13 19:16:57 +0800

    Прописаны эндпоинты: DELETE/ads/{id}removeAds GET/ads/{id}getAds PATCH/ads/{id}updateAds POST/usersaddUser

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -31,6 +31,46 @@ import java.util.List;
 @RestController
 @CrossOrigin(value = "http://localhost:3000")
 @RequestMapping("/ads")
+public class AdsController {
+    private Logger logger = LoggerFactory.getLogger(AdsController.class);
+
+    @GetMapping(value = "/{id}")
+    public FullAdsDto getAds(@PathVariable("id") long adsId) {
+        logger.info("Precessing getAds Controller");
+        FullAdsDto fullAdsDto = new FullAdsDto(); // Service getMap
+
+//        if (null == fullAdsDto) {
+//            throw new ResponseStatusException(
+//                    HttpStatus.NOT_FOUND
+//            );
+//        }
+        return fullAdsDto;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public Optional<HttpStatus> removeAds(@PathVariable("id") long adsId) {
+        logger.info("Processing removeAds Controller");
+        boolean isRemoveAds = false; // метод Service remove(adsId)
+
+        if (!isRemoveAds) {
+            return Optional.empty();
+        }
+
+        return Optional.of(HttpStatus.NO_CONTENT);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public AdsDto updateAds(@PathVariable("id") long adsId, @RequestBody AdsDto updatedAdsDto) {
+        logger.info("Processing updateAds Controller");
+        AdsDto adsDto = new AdsDto();// Находим объявление Service findById
+//        if (null == adsDto) {
+//            throw new ResponseStatusException(
+//                    HttpStatus.NOT_FOUND
+//            );
+//        }
+        //Service обновления объявления в БД, возвращаем обновленный объект
+        return updatedAdsDto;
+    }
 
 
 public class AdsController {
