788e9c8c6c2ce718762222917dee95c1d9779015
DanielNigrab <bgrln347@gmail.com>
2023-02-18 10:31:51 +0300

    Test UserServiceImplTest

--- src/test/java/ru/skypro/homework/service/UserServiceImplTest.java
+++ src/test/java/ru/skypro/homework/service/UserServiceImplTest.java
@@ -1,6 +1,5 @@
 package ru.skypro.homework.service;
 
-/*
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,13 +29,12 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
-*/
 
-  /*  @Mock
+    @Mock
     private UserRepository userRepository;
 
-   // @Spy
-   // private UserMapper userMapper = new UserMapperImpl();
+    @Spy
+    private UserMapper userMapper = new UserMapperImpl();
 
     @InjectMocks
     private UserServiceImpl userService;
@@ -116,4 +114,3 @@ public class UserServiceImplTest {
 
     }
 }
-*/
